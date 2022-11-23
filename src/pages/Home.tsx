55fd2ee4e37c86630c1f8da90504a70e72d2d4ce
algomonk016 <stshandilya0820@gmail.com>
2022-11-23 07:12:30 +0530

    added localstorage to save last location of model

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { Header, Display3dModel } from "components/";
+import { COORDS } from 'constant'
 
 import { ModelPosition } from "components/Home/types";
 
 const Home = (): JSX.Element => {
   const ModelPath = 'https://d1a370nemizbjq.cloudfront.net/2c761087-41ea-4774-9f9b-b696dd7c4e01.glb';
 
-  const [modelPosition, setModelPosition] = useState<ModelPosition>({x: 0, y: 0})
+  const [modelPosition, setModelPosition] = useState<ModelPosition>((!!localStorage.getItem(COORDS)) ? JSON.parse(localStorage.getItem(COORDS)!) : {x: 0, y: 0})
   const [modelSize, setModelSize] = useState<number>(250);
 
   const updateModelPosition = (newPosition: ModelPosition) => {
     setModelPosition(newPosition);
+    localStorage.setItem(COORDS, JSON.stringify(newPosition));
   }
 
   const updateModelSize = (newModelSize: number) => {
